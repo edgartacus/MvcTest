60c86da4aa7e6d784ca841c6f35e491987fa1478
Edgar <edgonaldo15@hotmail.com>
2014-03-27 11:50:48 -0600

    jQueryUIController

---
+++ MvcTest/Controllers/jQueryUIController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTest.Controllers
+{
+    public class jQueryUIController : Controller
+    {
+        //
+        // GET: /JqueryUi/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Datepicker()
+        {
+            return View();
+        }
+
+    }
+}
