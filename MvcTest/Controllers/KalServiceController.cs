0013002177835f71f7747c2a8ea1e0bba8d90eb8
Edgar <edgonaldo15@hotmail.com>
2014-04-14 17:51:31 -0500

    Autocomplete KalService

---
+++ MvcTest/Controllers/KalServiceController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTest.Controllers
+{
+    public class KalServiceController : Controller
+    {
+        //
+        // GET: /KalService/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PaqComplete()
+        {
+            return View();
+        }
+    }
+}
